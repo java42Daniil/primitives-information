a2fab6ed95d1fa797e9b1947cebf1ff71ff9dbb3
Nixter77 <61430177+Nixter77@users.noreply.github.com>
2021-10-10 23:25:49 +0300

    Update PrimitivesinformationAppl.java

--- Eclipse-Java/primitives-information-daniil/src/PrimitivesinformationAppl.java
+++ Eclipse-Java/primitives-information-daniil/src/PrimitivesinformationAppl.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 //Application for printing out the useful information about Java integer primitives
 public class PrimitivesinformationAppl {
